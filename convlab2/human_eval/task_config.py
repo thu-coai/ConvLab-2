83a90747e73d876fb0bdb70f56d23478fd7d7488
Jinchao Li <jincli@microsoft.com>
2020-09-09 22:50:57 -0700

    change task config

--- convlab2/human_eval/task_config.py
+++ convlab2/human_eval/task_config.py
@@ -26,17 +26,19 @@ and on the left side of the chat page. Supports HTML formatting.
 task_config['task_description'] = \
     """
     (You can keep accepting new HITs after you finish your current one, so keep working on it if you like the task!)
-    <br>
-    
-    <span id="user-goal" style="font-size: 16px;"> 
+    <br><br>
+
+    <b>In this task you will chat with an information desk clerk bot to plan your tour according to a given goal.</b>
+
+    <span id="user-goal" style="font-size: 16px;">
     </span>
-    
+
     <br><br>
     Chat with the bot naturally and stick to your own goal but <b>do not trivially copy the goal descriptions into the message.</b>
     <br>
-    Once the conversation is done, you will be asked to rate the bot on metrics like <b>goal accomplishment, language understanding, and response naturalness</b>.
+    <b>You can terminate the dialogue session by accessing the dialogue and typing 'success' or 'fail'. </b> Once the conversation is done, you will be asked to rate the bot on metrics like <b>goal accomplishment, language understanding, and response naturalness</b>.
     <br>
-    There is a <b>2 min</b> time limit for each turn.
+    There is a <b>2.5 min</b> time limit for each turn.
     <br>
     <br>
     - Do not reference the task or MTurk itself during the conversation.
@@ -44,9 +46,38 @@ task_config['task_description'] = \
     <b><span style="color:red">- No racism, sexism or otherwise offensive comments, or the submission will be rejected and we will report to Amazon.</b></span>
     <br>
     <br>
-    
+
+    <br>
+    For example, your given goal and expected conversation could be: <br><br>
+    <table border="1" cellpadding="10">
+    <tr><th>Your goal</th><th>Expected conversation</th></tr>
+    <tr><td>
+    <ul>
+    <li>You are looking for a <b>place to stay</b>. The hotel should be in the <b>cheap</b> price range and should be in the type of <b>hotel</b></li>
+    <li>The hotel should include <b>free parking</b> and should include <b>free wifi</b></li>
+    <li>Once you find the hotel, you want to book it for <b>6</b> people and <b>3</b> nights</b> starting from <b>tuesday</b></li>
+    <li>If the booking fails how about <b>2</b> nights</li>
+    <li>Make sure you get the <b>reference number</b></li>
+    </ul>
+    </td>
+    <td>
+    <b>You: </b>I am looking for a place to to stay that has cheap price range it should be in a type of hotel<br>
+    <b>Info desk: </b>Okay, do you have a specific area you want to stay in?<br>
+    <b>You: </b>no, i just need to make sure it's cheap. oh, and i need parking<br>
+    <b>Info desk: </b>I found 1 cheap hotel for you that includes parking. Do you like me to book it?<br>
+    <b>You: </b>Yes, please. 6 people 3 nights starting on tuesday.<br>
+    <b>Info desk: </b>I am sorry but I wasn't able to book that for you for Tuesday. Is there another day you would like to stay or perhaps a shorter stay?<br>
+    <b>You: </b>how about only 2 nights.<br>
+    <b>Info desk: </b>Booking was successful.\nReference number is : 7GAWK763. Anything else I can do for you?<br>
+    <b>You: </b>No, that will be all. Good bye.<br>
+    <b>Info desk: </b>Thank you for using our services.<br>
+    </td>
+    </table>
+
+
+
     <script type="text/javascript">
-    
+
     function handle_new_message(new_message_id, message) {
       var agent_id = message.id;
       var message_text = message.text
