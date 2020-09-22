d0ec9473d25c44f34de45f770437e4ec1bffcfb8
罗崚骁 <function2@qq.com>
2020-09-22 19:08:34 +0800

    revise xldst evaluation (#124)
    * update sumbt translation train result with evaluation mode set
    
    * update extract values
    
    * automatically download sumbt model
    
    * dstc9 eval
    
    * dstc9 xldst evaluation
    
    * modify example
    
    * add .gitignore
    
    * remove precision, recall, f1
    
    * release 250 test data
    
    * revise evaluation
    
    * fix file submission example

--- convlab2/dst/dstc9/eval_file.py
+++ convlab2/dst/dstc9/eval_file.py
@@ -2,28 +2,46 @@
     evaluate output file
 """
 
-from convlab2.dst.dstc9.utils import prepare_data, eval_states
+import os
+import json
+
+from convlab2.dst.dstc9.utils import prepare_data, extract_gt, eval_states, get_subdir
+
+
+def evaluate(model_dir, subtask, gt):
+    subdir = get_subdir(subtask)
+    results = {}
+    for i in range(1, 6):
+        filepath = os.path.join(model_dir, subdir, f'submission{i}.json')
+        if not os.path.exists(filepath):
+            continue
+        pred = json.load(open(filepath))
+        results[i] = eval_states(gt, pred)
+
+    json.dump(results, open(os.path.join(model_dir, subdir, 'file-results.json'), 'w'), indent=4, ensure_ascii=False)
+
+
+def dump_example(subtask, split):
+    test_data = prepare_data(subtask, split)
+    gt = extract_gt(test_data)
+    json.dump(gt, open(os.path.join('example', get_subdir(subtask), 'submission1.json'), 'w'), ensure_ascii=False, indent=4)
+    for dialog_id, states in gt.items():
+        for state in states:
+            for domain in state.values():
+                for slot in domain:
+                    domain[slot] = ""
+    json.dump(gt, open(os.path.join('example', get_subdir(subtask), 'submission2.json'), 'w'), ensure_ascii=False, indent=4)
+
 
 if __name__ == '__main__':
-    import os
-    import json
     from argparse import ArgumentParser
     parser = ArgumentParser()
     parser.add_argument('subtask', type=str, choices=['multiwoz', 'crosswoz'])
+    parser.add_argument('split', type=str, choices=['train', 'val', 'test', 'human_val'])
     args = parser.parse_args()
-
-    gt = {
-        dialog_id: [state for _, _, state in turns]
-        for dialog_id, turns in prepare_data(args.subtask).items()
-    }
-    # json.dump(gt, open('gt-crosswoz.json', 'w'), ensure_ascii=False, indent=4)
-
-    results = {}
-    for i in range(1, 6):
-        filename = f'submission{i}.json'
-        if not os.path.exists(filename):
-            continue
-        pred = json.load(open(filename))
-        results[filename] = eval_states(gt, pred)
-
-    json.dump(results, open('results.json', 'w'), indent=4, ensure_ascii=False)
+    subtask = args.subtask
+    split = args.split
+    dump_example(subtask, split)
+    test_data = prepare_data(subtask, split)
+    gt = extract_gt(test_data)
+    evaluate('example', subtask, gt)
