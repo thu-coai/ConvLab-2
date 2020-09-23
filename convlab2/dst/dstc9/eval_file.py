0b50be1b5c66e699bb76cbdc213888f70ad77d20
function2 <function2@qq.com>
2020-09-23 11:33:08 +0800

    update precision, recall, f1 calculation

--- convlab2/dst/dstc9/eval_file.py
+++ convlab2/dst/dstc9/eval_file.py
@@ -4,6 +4,7 @@
 
 import os
 import json
+from copy import deepcopy
 
 from convlab2.dst.dstc9.utils import prepare_data, extract_gt, eval_states, get_subdir
 
@@ -16,21 +17,35 @@ def evaluate(model_dir, subtask, gt):
         if not os.path.exists(filepath):
             continue
         pred = json.load(open(filepath))
-        results[i] = eval_states(gt, pred)
+        results[i] = eval_states(gt, pred, subtask)
 
+    print(json.dumps(results, indent=4))
     json.dump(results, open(os.path.join(model_dir, subdir, 'file-results.json'), 'w'), indent=4, ensure_ascii=False)
 
 
+# generate submission examples
 def dump_example(subtask, split):
     test_data = prepare_data(subtask, split)
-    gt = extract_gt(test_data)
-    json.dump(gt, open(os.path.join('example', get_subdir(subtask), 'submission1.json'), 'w'), ensure_ascii=False, indent=4)
-    for dialog_id, states in gt.items():
+    pred = extract_gt(test_data)
+    json.dump(pred, open(os.path.join('example', get_subdir(subtask), 'submission1.json'), 'w'), ensure_ascii=False, indent=4)
+    import random
+    for dialog_id, states in pred.items():
+        for state in states:
+            for domain in state.values():
+                for slot, value in domain.items():
+                    if value:
+                        if random.randint(0, 2) == 0:
+                            domain[slot] = ""
+                    else:
+                        if random.randint(0, 4) == 0:
+                            domain[slot] = "2333"
+    json.dump(pred, open(os.path.join('example', get_subdir(subtask), 'submission2.json'), 'w'), ensure_ascii=False, indent=4)
+    for dialog_id, states in pred.items():
         for state in states:
             for domain in state.values():
                 for slot in domain:
                     domain[slot] = ""
-    json.dump(gt, open(os.path.join('example', get_subdir(subtask), 'submission2.json'), 'w'), ensure_ascii=False, indent=4)
+    json.dump(pred, open(os.path.join('example', get_subdir(subtask), 'submission3.json'), 'w'), ensure_ascii=False, indent=4)
 
 
 if __name__ == '__main__':
