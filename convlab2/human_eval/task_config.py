ccda4dae228cf010f560d4d681771270849c6882
Jinchao Li <jincli@microsoft.com>
2020-09-25 22:46:19 -0700

    encapsule PipelineAgent internal state interface for return and replacement

--- convlab2/human_eval/task_config.py
+++ convlab2/human_eval/task_config.py
@@ -7,18 +7,18 @@ task_config = {}
 On the Amazon Mechanical Turk web site, the HIT title appears in search results,
 and everywhere the HIT is mentioned.
 """
-task_config['hit_title'] = 'Chat and evaluate bot!'
+task_config['hit_title'] = 'Chat and evaluate bot with a specified goal'
 
 """A description includes detailed information about the kind of task the HIT contains.
 On the Amazon Mechanical Turk web site, the HIT description appears in the expanded
 view of search results, and in the HIT and assignment screens.
 """
-task_config['hit_description'] = 'You will chat to a tour information bot and then evaluate that bot.'
+task_config['hit_description'] = 'You will chat to a tour information bot and then evaluate that bot, type "success" or "fail" once finished'
 
 """One or more words or phrases that describe the HIT, separated by commas.
 On MTurk website, these words are used in searches to find HITs.
 """
-task_config['hit_keywords'] = 'chat,dialog'
+task_config['hit_keywords'] = 'chat,dialog, dialogue, evaluation'
 
 """A detailed task description that will be shown on the HIT task preview page
 and on the left side of the chat page. Supports HTML formatting.
@@ -26,26 +26,19 @@ and on the left side of the chat page. Supports HTML formatting.
 task_config['task_description'] = \
     """
     (You can keep accepting new HITs after you finish your current one, so keep working on it if you like the task!)
-    <br><br>
-
-    <b>In this task you will chat with an information desk clerk bot to plan your tour according to a given goal.</b>
-
+    <br>
+    <br>
+    Chat with the bot naturally and stick to your own goal but do not trivially copy the goal descriptions into the message.
+    <br>
+    <br>
+    You can start with the conversation with sentences like <b>'I am looking for / I need to/ etc.'</b> and terminate the dialog session by typing <b> 'success' or 'fail'</b>. Once the conversation is done, you will be asked to rate the bot on metrics like goal accomplishment, language understanding, and response naturalness.
+    <br>
+    There is a <b>3 min</b> time limit for each turn.
+    <br>
+    <br>
+    In this task you will chat with an information desk clerk bot to plan your tour according to a given goal.
     <span id="user-goal" style="font-size: 16px;">
     </span>
-
-    <br><br>
-    Chat with the bot naturally and stick to your own goal but <b>do not trivially copy the goal descriptions into the message.</b>
-    <br>
-    <b>You can terminate the dialogue session by accessing the dialogue and typing 'success' or 'fail'. </b> Once the conversation is done, you will be asked to rate the bot on metrics like <b>goal accomplishment, language understanding, and response naturalness</b>.
-    <br>
-    There is a <b>2.5 min</b> time limit for each turn.
-    <br>
-    <br>
-    - Do not reference the task or MTurk itself during the conversation.
-    <br>
-    <b><span style="color:red">- No racism, sexism or otherwise offensive comments, or the submission will be rejected and we will report to Amazon.</b></span>
-    <br>
-    <br>
 
     <br>
     For example, your given goal and expected conversation could be: <br><br>
@@ -71,9 +64,15 @@ task_config['task_description'] = \
     <b>Info desk: </b>Booking was successful.\nReference number is : 7GAWK763. Anything else I can do for you?<br>
     <b>You: </b>No, that will be all. Good bye.<br>
     <b>Info desk: </b>Thank you for using our services.<br>
+    <b>You: </b>Success<br>
     </td>
     </table>
 
+
+    - Do not reference the task or MTurk itself during the conversation.
+    <br>
+    <b><span style="color:red">- No racism, sexism or otherwise offensive comments, or the submission will be rejected and we will report to Amazon.</b></span>
+    <br>
 
 
     <script type="text/javascript">
