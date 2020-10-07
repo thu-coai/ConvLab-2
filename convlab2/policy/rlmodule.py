45f0e1606925a44e0a1d55e46a6719941215f72a
Bruno Eidi Nishimoto <bruno_nishimoto@hotmail.com>
2020-10-06 23:37:39 -0300

    DQN (#113)
    * implemented script to extract all the statistics for all dialogue_act in data
    
    * changed script for actions be compatible to sys_da_voc.txt actions
    
    * multiwoz vector now supports composite actions
    
    * implemented ReplayMemory and EpsilongGreedyPolicy
    
    * implemented a basic version of dqn
    
    * included some comments

--- convlab2/policy/rlmodule.py
+++ convlab2/policy/rlmodule.py
@@ -56,7 +56,64 @@ class DiscretePolicy(nn.Module):
 
         return log_prob
  
-    
+class EpsilonGreedyPolicy(nn.Module):
+    def __init__(self, s_dim, h_dim, a_dim, epsilon_spec={'start': 0.1, 'end': 0.0, 'end_epoch': 200}):
+        super(EpsilonGreedyPolicy, self).__init__()
+
+        self.net = nn.Sequential(nn.Linear(s_dim, h_dim),
+                                 nn.ReLU(),
+                                 nn.Linear(h_dim, h_dim),
+                                 nn.ReLU(),
+                                 nn.Linear(h_dim, a_dim))
+
+        self.epsilon = epsilon_spec['start']
+        self.start = epsilon_spec['start']
+        self.end = epsilon_spec['end']
+        self.end_epoch = epsilon_spec['end_epoch']
+        self.a_dim = a_dim
+
+    def forward(self, s):
+        # [b, s_dim] => [b, a_dim]
+        a_weights = self.net(s)
+
+        return a_weights
+
+    def select_action(self, s, is_train=True):
+        """
+        :param s: [s_dim]
+        :return: [1]
+        """
+        # forward to get action probs
+        # [s_dim] => [a_dim]
+
+        if is_train:
+            if self.epsilon > np.random.rand():
+                # select a random action
+                a = torch.randint(self.a_dim, (1, ))
+            else:
+                a = self._greedy_action(s)
+        else:
+            a = self._greedy_action(s)
+
+        # transforms action index to a vector action (one-hot encoding)
+        a_vec = torch.zeros(self.a_dim)
+        a_vec[a] = 1.
+
+        return a_vec
+
+    def update_epsilon(self, epoch):
+        # Linear decay
+        a = -float(self.start - self.end) / self.end_epoch
+        b = float(self.start)
+        self.epsilon = max(self.end, a * float(epoch) + b)
+
+    def _greedy_action(self, s):
+        """
+        Select a greedy action
+        """
+        a_weights = self.forward(s)
+        return a_weights.argmax(0, True)
+
 class MultiDiscretePolicy(nn.Module):
     def __init__(self, s_dim, h_dim, a_dim):
         super(MultiDiscretePolicy, self).__init__()
@@ -224,3 +281,42 @@ class Memory(object):
 
     def __len__(self):
         return len(self.memory)
+
+class MemoryReplay(object):
+    """
+        The difference to class Memory is that MemoryReplay has a limited size.
+        It is mainly used for off-policy algorithms.
+    """
+    def __init__(self, max_size):
+        self.memory = []
+        self.index = 0
+        self.max_size = max_size
+
+    def push(self, *args):
+        """Saves a transition."""
+        if len(self.memory) < self.max_size:
+            self.memory.append(None)
+        self.memory[self.index] = Transition(*args)
+        self.index = (self.index + 1) % self.max_size
+        
+
+    def get_batch(self, batch_size=None):
+        if batch_size is None:
+            return Transition(*zip(*self.memory))
+        else:
+            random_batch = random.sample(self.memory, batch_size)
+            return Transition(*zip(*random_batch))
+
+    def append(self, new_memory):
+        for transition in new_memory.memory:
+            if len(self.memory) < self.max_size:
+                self.memory.append(None)
+            self.memory[self.index] = transition
+            self.index = (self.index + 1) % self.max_size
+
+    def reset(self):
+        self.memory = []
+        self.index = 0
+
+    def __len__(self):
+        return len(self.memory)        
